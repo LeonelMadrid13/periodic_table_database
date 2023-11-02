2a1a37e79e1d21921069ff048a659f0f0b3668fe
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2023-11-01 21:07:18 -0400

    feat: element.sh no argument check

---
+++ element.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
+
+if [[ -z $1 ]]
+then
+ echo -e "Please provide an element as an argument."
+ exit
+fi
