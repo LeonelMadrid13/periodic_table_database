3dc515d170cdfac12b2f7565abb67391c4f7289e
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2023-11-01 21:08:22 -0400

    feat: Get data from PSQL

--- element.sh
+++ element.sh
@@ -7,3 +7,10 @@ then
  echo -e "Please provide an element as an argument."
  exit
 fi
+
+if [[ $1 =~ ^[1-9]+$ ]]
+then
+  element=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties using(atomic_number) JOIN types USING(type_id) WHERE atomic_number = '$1'")
+else
+  element=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties using(atomic_number) JOIN types USING(type_id) WHERE name = '$1' or symbol = '$1'")
+fi
